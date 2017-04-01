26f1fee6d5d7df5b37b6bf83643c10fe18df99b9
zeffect <625417632@qq.com>
2017-04-01 10:05:42 +0800

    简单的写点匹配规则

--- app/src/main/java/apk/zeffect/cn/matchingdirs/MainActivity.java
+++ app/src/main/java/apk/zeffect/cn/matchingdirs/MainActivity.java
@@ -23,7 +23,10 @@ public class MainActivity extends Activity {
         DisplayMetrics dm = getResources().getDisplayMetrics();
         appendText("当前设备分辨率为：" + dm.widthPixels + "x" + dm.heightPixels + "(没有加上虚拟按键的高度)");
         appendText("当前设备DPI为：" + dm.densityDpi + "dpi" + "，为mdpi的" + dm.density + "倍");
-        appendText("当前设备匹配文件夹为：" + getResources().getString(R.string.values_match)+",(平板只匹配了sw600dp的文件夹)");
+        appendText("当前设备匹配文件夹为：" + getResources().getString(R.string.values_match) + ",(平板只匹配了sw600dp的文件夹)");
+        appendText("简单说明一下匹配规则：120->160dpi=ldpi,160->240dpi=mdpi,240-320dpi=hdpi,320->480dpi=xhdpi,480->640dpi=xxhpdi,>640dpi=xxxhdpi");
+        appendText("匹配sw<N>dp的规则：用最短的边如：1280*800,240dpi的平板，那么应该匹配到：800f/(240f/160)=533dp,基线160dpi,那么这个平板应该能匹配到小于sw533dp的文件夹");
+        appendText("其它的一些限定符：比如屏幕宽高，尺寸，方向之类的，都是能找到就找，找不到就找别的。");
     }
 
 
